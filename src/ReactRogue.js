504c6087ef205b1f974ffbe500437f026079f043
quinn5moli <quinn.moli@gmail.com>
2021-12-27 15:02:29 -0800

    add player state; black square can move via keys

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -1,12 +1,18 @@
-import React, {useRef, useEffect} from "react";
+import React, {useRef, useEffect, useState} from "react";
 import InputManager from "./InputManager";
 
 const ReactRogue = ({width, height, tilesize}) => {
     const canvasRef = useRef();
+    const [player, setPlayer] = useState({ x: 64, y: 128 })
     let inputManager= new InputManager();
     const handleInput = (action, data) => {
         console.log(`handle input: ${action}:${JSON.stringify(data)}`);
+        let newPlayer = { ...player };
+        newPlayer.x += data.x * tilesize;
+        newPlayer.y += data.y * tilesize;
+        setPlayer(newPlayer);
     };
+
     useEffect(() => {
         console.log('Bind input');
         inputManager.bindKeys();
@@ -22,7 +28,7 @@ const ReactRogue = ({width, height, tilesize}) => {
         const ctx = canvasRef.current.getContext('2d');
         ctx.clearRect(0,0, width * tilesize, height * tilesize);
         ctx.fillStyle = '#000';
-        ctx.fillRect(12, 22, 16, 16);
+        ctx.fillRect(player.x, player.y, 16, 16);
     });
     return (
         <canvas 
