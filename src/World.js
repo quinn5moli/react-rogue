1e701ca451600a9aafb78b5ed9d5169e2a6bf5f8
quinn5moli <quinn.moli@gmail.com>
2021-12-27 19:37:31 -0800

    loot populates on proper surface and when play bumps into loot it registers in the console

--- src/World.js
+++ src/World.js
@@ -41,9 +41,22 @@ class World {
             this.worldmap[x][y] === 1)
     }
 
+    getEntityAtLocation(x, y){
+        return this.entities.find(entity => entity.x === x && entity.y === y);
+    }
+
     movePlayer(dx, dy){
         let tempPlayer = this.player.copyPlayer();
         tempPlayer.move(dx, dy);
+        let entity = this.getEntityAtLocation(tempPlayer.x, tempPlayer.y);
+        if(entity) {
+            console.log(entity);
+            entity.action('bump', this);
+            return;
+        }
+
+
+
         if(this.isWall(tempPlayer.x, tempPlayer.y)){
             console.log(`Way blocked at ${tempPlayer.x}:${tempPlayer.y}!`);
         } else {
