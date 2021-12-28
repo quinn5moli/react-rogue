d12b703c9f74195eaf77c57e679bab138c5eebf1
quinn5moli <quinn.moli@gmail.com>
2021-12-27 17:15:30 -0800

    world component started and integrated with ReactRogue; need to pretty up the world/map

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -1,10 +1,12 @@
 import React, {useRef, useEffect, useState} from "react";
 import InputManager from "./InputManager";
 import Player from "./Player";
+import World from "./World";
 
 const ReactRogue = ({width, height, tilesize}) => {
     const canvasRef = useRef();
     const [player, setPlayer] = useState(new Player(1,2, tilesize));
+    const [world, setWorld] = useState( new World(width, height, tilesize));
     let inputManager= new InputManager();
     const handleInput = (action, data) => {
         console.log(`handle input: ${action}:${JSON.stringify(data)}`);
@@ -28,6 +30,7 @@ const ReactRogue = ({width, height, tilesize}) => {
         console.log('Draw to canvas');
         const ctx = canvasRef.current.getContext('2d');
         ctx.clearRect(0,0, width * tilesize, height * tilesize);
+        world.draw(ctx);
         player.draw(ctx);
     });
     return (
